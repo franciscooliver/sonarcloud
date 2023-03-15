d184c3bb22a6c353d1c1a8cec2f01faff2600543
Francisco Alves <francisco.alves@oncologiador.corp>
2023-03-15 20:57:46 -0300

    ci: check if quality gate is working on sonar cloud

--- sum.go
+++ sum.go
@@ -10,14 +10,14 @@ func sum(a int, b int) int {
 	return a + b
 }
 
-func sub(a int, b int) int {
-	return a - b
-}
-
-func mult(a int, b int) int {
-	return a * b
-}
-
-func div(a int, b int) int {
-	return a / b
-}
+//func sub(a int, b int) int {
+//	return a - b
+//}
+//
+//func mult(a int, b int) int {
+//	return a * b
+//}
+//
+//func div(a int, b int) int {
+//	return a / b
+//}
